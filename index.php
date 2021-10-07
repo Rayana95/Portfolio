bccadcd0e9d5dec2f58381843a743169523c26b9
Rayana95 <raykai2001@gmail.com>
2021-10-07 14:06:22 +0200

    mise en place de bootstrap

--- index.php
+++ index.php
@@ -2,3 +2,46 @@
 
 require_once 'config/framework.php';
 require_once 'config/connect.php';
+?>
+
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <div id="header-top">
+        
+        <!-- /// HEADER TOP  //////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+        
+            <div class="ewf-row">
+                <div class="ewf-span6" id="header-top-widget-area-1">
+				<div id="ewf_widget_contact_info-3" class="widget ewf_widget_contact_info"><ul><li><i class="ifc-phone1"></i>+33 6 95 06 96 22</li><li><i class="ifc-email"></i>raykai2001@gmail.com</li></ul></div>                </div><!-- end .span6 -->
+
+                <div class="ewf-span6" id="header-top-widget-area-2">
+				<div id="ewf_widget_social_media-2" class="widget ewf_widget_social_media"><div class="fixed"><a class="facebook-icon social-icon" href="https://www.facebook.com/pages/Alsace-Web/1500717453514496"><i class="fa fa-facebook"></i></a><a class="twitter-icon social-icon" href="https://twitter.com/AlsaceWeb"><i class="fa fa-twitter"></i></a><a class="googleplus-icon social-icon" href="https://plus.google.com/u/0/117783402578598763927/about"><i class="fa fa-google-plus"></i></a><a class="instagram-icon social-icon" href="http://instagram.com/alsaceweb/"><i class="fa fa-instagram"></i></a></div></div>                </div><!-- end .span6 -->
+            </div><!-- end .row -->
+            
+        <!-- //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+            
+        </div><!-- end #header-top -->
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.min.js" integrity="sha384-PsUw7Xwds7x08Ew3exXhqzbhuEYmA2xnwc8BuD6SEr+UmEHlX8/MCltYEodzWA4u" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
